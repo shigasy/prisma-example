1adbc600a8d6d922e439a797ff04132c841c4e9b
shigasy <kxx555u.u@gmail.com>
2021-02-09 15:36:06 +0900

    add: リレーションされたデータも取得。prismaの生成される型が強力。allUsersにカーソルを合わせると、型が出力される

--- starter/script.ts
+++ starter/script.ts
@@ -5,8 +5,8 @@ const prisma = new PrismaClient()
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers);
+  const allUsers = await prisma.user.findMany({ include: { posts: true }})
+  console.dir(allUsers, { depth: null })
   
 }
 
